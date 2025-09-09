0570182dcf7ee9f3269d4919fcee1aa53e2d6ab5
jfmaes <jeanfrancoismaes.limited@gmail.com>
2025-09-09 22:08:13 +0100

    deving

---
+++ privesc-workshop/prod/terraform/modules/instances/variables.tf
@@ -0,0 +1,46 @@
+variable "instance_prefix" {
+  type        = string
+  default     = "SEC565"
+}
+
+variable "ami_owner" {
+  type        = string
+  default     = "201554634916"
+}
+
+variable "private_subnet_sandbox_PWNZONE_id" {
+  type        = string
+  description = "ID of the private subnet for sandbox PWNZONE"
+}
+
+variable "private_subnet_PWNZONE_id" {
+  type        = string
+  description = "ID of the private subnet for PWNZONE"
+}
+
+variable "private_subnet_TREASUREISLAND_id" {
+  type        = string
+  description = "ID of the private subnet for TREASUREISLAND"
+}
+
+variable "lab_security_group_id" {
+  type        = string
+  description = "ID of the lab security group"
+}
+
+variable "key_pair_name" {
+  type        = string
+  description = "Name of the key pair to ssh into boxes."
+}
+
+variable "instance_type" {
+  type        = string
+  default = "t2.medium"
+}
+
+
+variable "lab_version_tag" {
+  type        = string
+  default = "v0.1"
+}
+
---
+++ privesc-workshop/prod/terraform/vars.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+
+variable "lab_version_tag" {
+  type    = string
+  default = "v0.1"
+}
